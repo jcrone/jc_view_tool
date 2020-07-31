f4bb08805ad42adf53def38cde6c187df651993d
Jamie Crone <jamiecrone@gmail.com>
2020-07-31 14:29:00 -0700

    practice gem

---
+++ lib/jc_view_tool.rb
@@ -0,0 +1,7 @@
+require "jc_view_tool/version"
+require "jc_view_tool/render"
+
+module JcViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jc_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JcViewTool
+  VERSION = "0.1.0"
+end
